816978444a85ae4440b80ba2d38f0570c406aeff
DavidDvpt <david.mosca69@gmail.com>
2020-10-14 19:49:43 +0200

    add actor and  description div

--- src/component/Character.jsx
+++ src/component/Character.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import '../css/twd.css';
+import React from "react";
+import "../css/twd.css";
 
 class Character extends React.Component {
-    render() {
-            
-        return (
-            <div className="characterBloc">
-                <figure>
-                    <figcaption>{this.props.firstName + " " + this.props.lastName}</figcaption>
-                    <img src={this.props.image} alt={this.props.firsName + " " + this.props.lastName} />
-                </figure>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="characterBloc">
+        <figure>
+          <figcaption>
+            {this.props.firstName + " " + this.props.lastName}
+          </figcaption>
+          <img
+            src={this.props.image}
+            alt={this.props.firsName + " " + this.props.lastName}
+          />
+        </figure>
+        <div className="actorBloc">
+            <h4>Acteur:</h4>
+        </div>
+        <div className="descriptionBloc">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatum commodi temporibus nostrum deserunt autem molestias maiores consectetur, sint exercitationem nisi dolores eligendi vitae eveniet quibusdam illo consequuntur blanditiis amet!</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Character
+export default Character;
--- src/component/characterData.js
+++ src/component/characterData.js
@@ -40,6 +40,60 @@ const characters = [
         lastName: "Grimes",
         image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
         description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
+    },,
+    {
+        firstName: "Rick",
+        lastName: "Grimes",
+        image: "https://64.media.tumblr.com/18876edb51636d20b6be9614f3e22a55/tumblr_phpyb1XoVs1tc5gvpo4_250.png",
+        description: "Hero !!"
     },
 ]
 
